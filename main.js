c99e3ab469219227bcaa96a02ea2dd4a6e4df2ee
dmcclellan3 <wdmcclellan3@gmail.com>
2024-03-21 11:47:54 -0400

    newest

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
   return array[random];
 }
-var storytext = "It was 94 fahrenheit outside, so :insertX: went for a walk. When they got to :insertY:, they stared in horror for a few moments, then :insertZ:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
+var storyText = "It was 94 fahrenheit outside, so :insertX: went for a walk. When they got to :insertY:, they stared in horror for a few moments, then :insertZ:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
 
 const insertX = ["Willy the Goblin","Big Daddy","Father Christmas"];
 
@@ -17,6 +17,18 @@ const insertZ = ["spontaneously combusted","melted into a puddle on the sidewalk
 randomize.addEventListener('click', result);
 
 function result() {
+
+    let newStory = storyText;
+
+var xItem = randomValueFromArray(insertX);
+var yItem = randomValueFromArray(insertY);
+var zItem = randomValueFromArray(insertZ);
+
+newStory = newStory.replaceAll(':insertX', xItem)
+newStory = newStory.replaceAll(':insertY', yItem)
+newStory = newStory.replaceAll(':insertZ', zitem)
+
+
 
   if(customName.value !== '') {
     const name = customName.value;
@@ -29,6 +41,6 @@ function result() {
 
   }
 
-  story.textContent = ;
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }
